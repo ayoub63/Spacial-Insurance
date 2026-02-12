09950d3f4fad3da0a09439c2d3ebd27ef1952e68
Adam-Ibra <adam.ibra2504@gmail.com>
2026-02-12 20:04:35 +0100

    .

--- frontend/Home.py
+++ frontend/Home.py
@@ -11,8 +11,16 @@ st.title("Spacial-Insurance Dashboard")
 
 st.sidebar.title("Einstellungen")
 tolerance = st.sidebar.slider("Max Risk Tolerance", 0, 100, 80)
-base_premium = st.sidebar.slider("Basis-Prämie in Euro", 0, 100000, 1000)
+base_premium = st.sidebar.number_input("Basis-Prämie in Euro", 0, 100000, 1000)
 
 st.write("Aktuelle Einstellungen: ")
-st.write("Tolerance: ", tolerance)
-st.write("Base premium: ", base_premium)
+st.write("Toleranz: ", tolerance)
+st.write("Basis Prämie: ", base_premium)
+
+
+col1, col2, col3, col4 = st.columns(4)
+
+col1.metric("Durchschn. Risk Score", "Platzhalter")
+col2.metric("Max. Risk Score", "platzhalter")
+col3.metric("Objekte > Toleranz", "platzhalter")
+col4.metric("Premien Summe (€)", "platzhalter")
