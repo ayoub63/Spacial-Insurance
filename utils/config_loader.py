3663dacc0c4b1501a039e493a7d3d61955e74d17
ayoub63 <ayoubafkir18@gmail.com>
2026-02-15 23:35:38 +0100

    README.md

--- utils/config_loader.py
+++ utils/config_loader.py
@@ -1,50 +1,34 @@
 # Author: Abuzar
 # utils/config_loader.py
 
-"""Lädt und verwaltet die YAML-Konfigurationsdatei."""
+# Lädt und verwaltet die YAML-Konfigurationsdatei
 
 import yaml
 
 
 class ConfigLoader:
-    """Lädt und verwaltet die YAML-Konfigurationsdatei."""
 
     def __init__(self, config_path: str):
-        """
-        Initialisiert den ConfigLoader.
-        :param config_path: Pfad zur YAML-Konfigurationsdatei
-        """
+        # Initialisiert den ConfigLoader
         self.config_path = config_path
         self.yaml_config = self._load_yaml()
 
     def _load_yaml(self):
-        """
-        Lädt die YAML-Datei.
-        :return: Dictionary mit Konfigurationen
-        """
+        # Lädt die YAML-Datei
         with open(self.config_path, "r", encoding="utf-8") as file:
             return yaml.safe_load(file)
 
     def get_database_path(self):
-        """
-        Gibt den Datenbank-Pfad zurück.
-        :return: Pfad zur SQLite-Datenbank
-        """
+        # Gibt den Datenbank-Pfad zurück.
         db = self.yaml_config.get("database", {})
         return db.get("path")
 
     def get_csv_path(self):
-        """
-        Gibt den CSV-Dateipfad zurück.
-        :return: Pfad zur CSV-Datei
-        """
+        # Gibt den CSV-Dateipfad zurück
         paths = self.yaml_config.get("paths", {})
         return paths.get("csv_raw")
 
     def get_create_table_query(self):
-        """
-        Gibt die CREATE TABLE Query zurück.
-        :return: SQL CREATE TABLE Statement
-        """
+        # Gibt die CREATE TABLE Query zurück
         queries = self.yaml_config.get("queries", {})
         return queries.get("create_table")
