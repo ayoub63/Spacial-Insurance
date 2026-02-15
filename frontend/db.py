299d4830a47e8a6a43f71c134c0058fbb0726e07
Abuzar388 <zaher1497@gmail.com>
2026-02-15 15:33:02 +0100

    sqlite

--- frontend/db.py
+++ frontend/db.py
@@ -1,8 +1,11 @@
 # Author: Adam Ibrahimkhel
 
 import sqlite3
+import os
+
 
 def get_db():
-    conn = sqlite3.connect('../data_src/processed/space_insurance.db')
+    base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    db_path = os.path.join(base_dir, 'data_src', 'processed', 'space_insurance.db')
+    conn = sqlite3.connect(db_path)
     return conn
-
